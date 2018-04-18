6f52d13bef226bb1f492ec7ab58a6819adb23f42
yinm <yinm.cminor9@gmail.com>
2018-04-19 08:38:59 +0900

    Check paragraph text

--- tutorial-part-one/src/pages/index.js
+++ tutorial-part-one/src/pages/index.js
@@ -1,3 +1,7 @@
 import React from "react"
 
-export default () => <div style={{ color: 'tomato' }}>Hello Gatsby!</div>
+export default () =>
+  <div style={{ color: 'tomato' }}>
+    <h1>Hello Gatsby!</h1>
+    <p>What a world.</p>
+  </div>
