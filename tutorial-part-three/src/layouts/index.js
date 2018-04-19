25a5e5577241c071b3f1557a97a1e8228c2f7eea
yinm <yinm.cminor9@gmail.com>
2018-04-19 23:45:36 +0900

    Add title

--- tutorial-part-three/src/layouts/index.js
+++ tutorial-part-three/src/layouts/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 export default ({ children }) => (
   <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0.1rem` }}>
+    <h3>MySweetSite</h3>
     {children()}
   </div>
 )
