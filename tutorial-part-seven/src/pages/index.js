146c1698b01be08bc1c7d960fd7d0c9744f059ab
yinm <yinm.cminor9@gmail.com>
2018-04-24 06:13:20 +0900

    Add link

--- tutorial-part-seven/src/pages/index.js
+++ tutorial-part-seven/src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import g from "glamorous"
+import Link from "gatsby-link"
 
 import { rhythm } from "../utils/typography"
 
@@ -13,11 +14,16 @@ export default ({ data }) => {
       <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
       {data.allMarkdownRemark.edges.map(({ node }) => (
         <div key={node.id}>
+          <Link
+            to={node.fields.slug}
+            css={{ textDecoration: `none`, color: `inherit` }}
+          >
           <g.H3 marginBottom={rhythm(1 / 4)}>
             {node.frontmatter.title}{" "}
             <g.Span color="#BBB">- {node.frontmatter.date}</g.Span>
           </g.H3>
           <p>{node.excerpt}</p>
+          </Link>
         </div>
       ))}
     </div>
@@ -34,6 +40,9 @@ export const query = graphql`
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
           }
           excerpt
         }
