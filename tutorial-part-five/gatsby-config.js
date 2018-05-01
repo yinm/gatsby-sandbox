57e42ece0f34f5ff33d5f54db3d863cbf357a626
yinm <yinm.cminor9@gmail.com>
2018-05-02 08:53:05 +0900

    Copy ch4 to ch5

--- tutorial-part-five/gatsby-config.js
+++ tutorial-part-five/gatsby-config.js
@@ -3,13 +3,6 @@ module.exports = {
     title: `Pandas Eating Lots`,
   },
   plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `src`,
-        path: `${__dirname}/src/`,
-      },
-    },
     `gatsby-plugin-glamor`,
     {
       resolve: `gatsby-plugin-typography`,
