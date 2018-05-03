a8e0727b60383c0d3de22e2aeb8ff3aca6f87717
yinm <yinm.cminor9@gmail.com>
2018-05-03 20:26:23 +0900

    Copy ch6 to ch7

--- tutorial-part-seven/src/pages/index.js
+++ tutorial-part-seven/src/pages/index.js
@@ -1,29 +1,24 @@
-import React from "react"
-import g from "glamorous"
-import Link from "gatsby-link"
+import React from 'react'
+import g from 'glamorous'
 
-import { rhythm } from "../utils/typography"
+import { rhythm } from '../utils/typography'
 
-export default ({ data }) => {
+export default({ data }) => {
   console.log(data)
   return (
     <div>
-      <g.H1 display={"inline-block"} borderBottom={"1px solid"}>
-        Amazing Pandas Eating Things
+      <g.H1 display={'inline-block'} borderBottom={'1px solid'}>
+        Amazing Pandas Eating things
       </g.H1>
       <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+
       {data.allMarkdownRemark.edges.map(({ node }) => (
         <div key={node.id}>
-          <Link
-            to={node.fields.slug}
-            css={{ textDecoration: `none`, color: `inherit` }}
-          >
           <g.H3 marginBottom={rhythm(1 / 4)}>
             {node.frontmatter.title}{" "}
-            <g.Span color="#BBB">- {node.frontmatter.date}</g.Span>
+            <g.Span color="#bbb">- {node.frontmatter.date}</g.Span>
           </g.H3>
           <p>{node.excerpt}</p>
-          </Link>
         </div>
       ))}
     </div>
@@ -32,19 +27,16 @@ export default ({ data }) => {
 
 export const query = graphql`
   query IndexQuery {
-    allMarkdownRemark {
-      totalCount
+    allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
       edges {
         node {
-          id
           frontmatter {
             title
-            date(formatString: "DD MMMM, YYYY")
-          }
-          fields {
-            slug
+            date
           }
           excerpt
+          timeToRead
+          html
         }
       }
     }
