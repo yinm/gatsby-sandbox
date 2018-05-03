eb47c9003999a5127c273b81a244ccb110d0723d
yinm <yinm.cminor9@gmail.com>
2018-05-03 21:15:09 +0900

    Add links to posts

--- tutorial-part-seven/src/pages/index.js
+++ tutorial-part-seven/src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import g from 'glamorous'
+import Link from 'gatsby-link'
 
 import { rhythm } from '../utils/typography'
 
@@ -14,11 +15,16 @@ export default({ data }) => {
 
       {data.allMarkdownRemark.edges.map(({ node }) => (
         <div key={node.id}>
-          <g.H3 marginBottom={rhythm(1 / 4)}>
-            {node.frontmatter.title}{" "}
-            <g.Span color="#bbb">- {node.frontmatter.date}</g.Span>
-          </g.H3>
-          <p>{node.excerpt}</p>
+          <Link
+            to={node.fields.slug}
+            css={{ textDecoration: 'none', color: 'inherit' }}
+          >
+            <g.H3 marginBottom={rhythm(1 / 4)}>
+              {node.frontmatter.title}{" "}
+              <g.Span color="#bbb">- {node.frontmatter.date}</g.Span>
+            </g.H3>
+            <p>{node.excerpt}</p>
+          </Link>
         </div>
       ))}
     </div>
@@ -33,6 +39,9 @@ export const query = graphql`
           frontmatter {
             title
             date
+          }
+          fields {
+            slug
           }
           excerpt
           timeToRead
