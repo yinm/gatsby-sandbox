918d3f08f4d98a022de1af92b713ddf67492c19e
yinm <yinm.cminor9@gmail.com>
2018-05-03 20:53:05 +0900

    Check getting slug with graphql

--- tutorial-part-seven/gatsby-node.js
+++ tutorial-part-seven/gatsby-node.js
@@ -1,6 +1,13 @@
-exports.onCreateNode = ({ node, getNode }) => {
+const { createFilePath } = require('gatsby-source-filesystem')
+
+exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
+  const { createNodeField } = boundActionCreators
   if (node.internal.type === 'MarkdownRemark') {
-    const fileNode = getNode(node.parent)
-    console.log('\n', fileNode.relativePath)
+    const slug = createFilePath({ node, getNode, basePath: 'pages' })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
   }
 }
