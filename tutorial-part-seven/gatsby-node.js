6f22330ce33298d8c87524923e74d24c447dd281
yinm <yinm.cminor9@gmail.com>
2018-05-03 20:36:37 +0900

    Check creating slug

--- tutorial-part-seven/gatsby-node.js
+++ tutorial-part-seven/gatsby-node.js
@@ -1,5 +1,6 @@
-exports.onCreateNode = ({ node }) => {
+exports.onCreateNode = ({ node, getNode }) => {
   if (node.internal.type === 'MarkdownRemark') {
-    console.log(node.internal.type)
+    const fileNode = getNode(node.parent)
+    console.log('\n', fileNode.relativePath)
   }
 }
